caf947791496c5bc80736da4e71f9a18c8de3abb
Сергей Лапшин <gustav474@gmail.com>
2021-03-29 17:52:24 +0400

    need refactoring

--- src/main/java/com/gustav474/NIOString/Main.java
+++ src/main/java/com/gustav474/NIOString/Main.java
@@ -6,13 +6,12 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         //TODO как правильно брать пути?
+        //TODO проверка переданного пути на существование
         Path pathToDir = Paths.get("/Users/gustav474/Desktop/Desktop – MacBook Pro — Сергей/DEV/Java/NIOString/src/main/resources/toDir");
         Path pathFromDir = Paths.get("/Users/gustav474/Desktop/Desktop – MacBook Pro — Сергей/DEV/Java/NIOString/src/main/resources/fromDir");
         Path pathToDict = Paths.get("/Users/gustav474/Desktop/Desktop – MacBook Pro — Сергей/DEV/Java/NIOString/src/main/resources/dict.txt");
 
         Parser parser = new Parser(pathToDir, pathFromDir, pathToDict);
         parser.parse();
-
-
     }
 }
--- src/main/java/com/gustav474/NIOString/Parser.java
+++ src/main/java/com/gustav474/NIOString/Parser.java
@@ -2,18 +2,18 @@ package com.gustav474.NIOString;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+
+import java.io.*;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.AsynchronousFileChannel;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -33,6 +33,7 @@ public class Parser {
 
     private void parseFromDir() {
         //TODO собрать инфу о файлах в этой директории
+        //TODO сделать кастомное Exception если нет ни одного текестового файла
         List<Path> filesInDir = null;
         try {
             filesInDir = Files.list(pathFromDir).collect(Collectors.toList());
@@ -43,21 +44,87 @@ public class Parser {
 
         //TODO Выводим через стрим данные из файла построчно
         for (Path path : filesInDir) {
-            try {
-                List<String> list = Files.readAllLines(path);
-                //Заменяем нужные слова
-//                List list = streamOfStrings.map(str -> str.replace(" " + "и" + " ", " " + "*" + " "))
-//                                .collect(Collectors.toList());
-                list.stream()
-                    .map(str -> str.replace(" " + "и" + " ", " " + "*" + " "))
-//                    .collect(Collectors.toList())
-                    .forEach(TestMR::printMR);
-//                Files.copy(pathToDir.resolve(path), streamOfStrings);
-//                System.out.println(outList);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            cleanup(path);
+            getTextByPath2(path);
         }
 
     }
+
+    private void getTextByPath (Path path) {
+        try {
+            Stream<String> list = Files.lines(path);
+            Stream<String> dict = Files.lines(pathToDict);
+
+            list
+                    //TODO слово может быть вхождением в другое
+                    //TODO брать слова из словаря
+                    .map(str -> str.replace(" " + "и" + " ", " " + "*" + " "))
+//                    .collect(Collectors.toList())
+                    //TODO складывать все это в другой файл, а не печатать
+                    .forEach(str -> {
+                        try {
+                            //TODO записывать только один раз в файл, если запускать парсер еще раз, дублирование текста в файле не происходит
+                            Files.write(Paths.get(pathToDir.toString(), path.getFileName().toString()), Collections.singleton(str), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+//            System.out.println(pathToDir.toString() + path.getFileName().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void getTextByPath2 (Path path) {
+        try {
+            BufferedReader text = Files.newBufferedReader(path);
+//            BufferedReader dict = Files.newBufferedReader(pathToDict);
+
+            while(true) {
+                String line = text.readLine();
+                System.out.println(line);
+                if (line == null) break;
+                StringBuilder str = new StringBuilder(line);
+
+                BufferedReader dict = Files.newBufferedReader(pathToDict);
+                String dic = dict.readLine();
+                if (dic != null) {
+
+                    System.out.println(dic);
+                    //TODO парсим слова из полученной строки
+                    List<String> words = Arrays.asList(dic.toString().split(" - "));
+//                    System.out.println(words);
+
+                    for (String word : words) {
+                        Integer index = str.indexOf(word);
+                        while(index != -1) {
+                            str.replace(index, index + word.length(), new String(new char[word.length()]).replace("\0", "*"));
+                            index = str.indexOf(word, index);
+                            System.out.println(str.toString());
+                        }
+                    }
+
+                    //TODO записывать только один раз в файл, если запускать парсер еще раз, дублирование текста в файле не происходит
+//                    try {
+                        Files.write(Paths.get(pathToDir.toString(), path.getFileName().toString()), Collections.singleton(str), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private void cleanup (Path path) {
+        try {
+            Files.delete(Paths.get(pathToDir.toString(), path.getFileName().toString()));
+            Files.createFile(Paths.get(pathToDir.toString(), path.getFileName().toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
